e28cac839d4e5bbdaec222a2fe333f746b928728
Ug0510 <uditgg0510@gmail.com>
2024-02-18 16:33:22 +0530

    Create Question Schema

---
+++ server/models/Question.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    options: {
+        type: [String],
+        required: true
+    },
+    correctAnswers: {
+        type: [String],
+        required: true
+    },
+    category: {
+        type: String,
+        default: 'unclassified'
+    },
+    difficultyLevel: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
