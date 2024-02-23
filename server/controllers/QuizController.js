e55355784e12d5cf0dadc05baa3c92cbbfc78a08
Ug0510 <uditgg0510@gmail.com>
2024-02-23 21:49:45 +0530

    create api to join quiz for students

--- server/controllers/QuizController.js
+++ server/controllers/QuizController.js
@@ -1,4 +1,5 @@
 const QuizGame = require('../models/QuizGame');
+const User = require('../models/User');
 
 
 // Controller function to generate a quiz
@@ -49,5 +50,42 @@ exports.generateQuiz = async (req, res) => {
       res.status(500).json({ message: 'Error generating quiz' });
     }
   };
+
+  
   
 
+ // Controller to join Quiz
+  exports.joinQuiz = async (req, res) => {
+    try {
+      // Extract quiz ID from URL parameter
+      const { quizId } = req.params;
+  
+      // Check if the quiz exists
+      const quiz = await QuizGame.findById(quizId);
+      if (!quiz) {
+        return res.status(404).json({ message: 'Quiz not found' });
+      }
+
+      // Check if quiz if started or not  
+      if (quiz.isStarted)
+      {
+        return res.status(403).json({message: 'Quiz is already started , Wait for next Quiz'});
+      }
+  
+      // Check if the user is a student
+      const user = await User.findById(req.user._id);
+      if (!user || user.userType !== 'student') {
+        return res.status(403).json({ message: 'Only students are allowed to join quizzes' });
+      }
+  
+      // Add the student to the list of participants (studentIds) in the quiz
+      quiz.studentIds.push(req.user._id);
+      await quiz.save();
+  
+      res.status(200).json({ message: 'Student joined the quiz successfully' });
+    } catch (error) {
+      console.error('Error joining quiz:', error);
+      res.status(500).json({ message: 'Error joining quiz' });
+    }
+  };
+  
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../../middleware/jwt_strategy');
 const UserController = require('../../controllers/UserController');
+const QuizController = require('../../controllers/QuizController');
 
 // Define routes for different CRUD operations
 router.post('/', UserController.createUser);
 router.get('/:userId', UserController.getUser);
 router.post('/login', UserController.loginUser);
 router.post('/profile', authenticate, UserController.profileUser);
+
+
+router.post('/join-quiz/:quizId',authenticate, QuizController.joinQuiz);
 module.exports = router;
