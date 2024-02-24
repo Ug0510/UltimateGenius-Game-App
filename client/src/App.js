0b8d0864a470a654af97b68d83ece4038f75f279
Ug0510 <uditgg0510@gmail.com>
2024-02-24 16:15:38 +0530

    Improve App.js structure for login authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import LoginPage from './pages/User/Login';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
-import RegisterPage from './pages/User/Register';
-import fetchUserData from './utils/fetchUserData';
-import checkIsLoggedIn from './utils/checkIsLoggedIn';
+import LoginPage from './pages/User/LoginPage';
+import RegisterPage from './pages/User/RegisterPage';
+import { checkIsLoggedIn } from './utils/authUtils';
 
 const App = () => {
-
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
-
-        checkIsLoggedIn({setIsLoggedIn});
-        
-      }, [isLoggedIn]);
-
+        checkIsLoggedIn(setIsLoggedIn);
+    }, []);
 
     return (
         <Router>
             <Routes>
-            <Route path="/" element={<Homepage isLoggedIn={isLoggedIn}  setIsLoggedIn={setIsLoggedIn}/>} />
-            <Route path="/user/login" element={<LoginPage isLoggedIn={isLoggedIn}  setIsLoggedIn={setIsLoggedIn}/>} />
-            <Route path="/user/register" element={<RegisterPage/>} />
+                <Route
+                    path="/"
+                    element={
+                        isLoggedIn ? (
+                            <Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+                        ) : (
+                            <Navigate to="/user/login" />
+                        )
+                    }
+                />
+                <Route
+                    path="/user/login"
+                    element={
+                        isLoggedIn ? (
+                            <Navigate to="/" />
+                        ) : (
+                            <LoginPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+                        )
+                    }
+                />
+                <Route path="/user/register" element={<RegisterPage />} />
             </Routes>
         </Router>
-            );
-}
+    );
+};
 
 export default App;
